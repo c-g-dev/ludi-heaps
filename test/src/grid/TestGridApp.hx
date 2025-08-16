4737c409df6c1a2957285b977c1fea5264af5025
cardgdev <cardgamedev@proton.me>
2025-08-16 14:39:45 -0500

    l

--- test/src/grid/TestGridApp.hx
+++ test/src/grid/TestGridApp.hx
@@ -10,7 +10,13 @@ import ludi.heaps.grid.Cell;
 import hxd.Res;
 import grid.FXAA.FXAAFilter;
 
+import h2d.Bitmap;
+import h2d.Tile;
+import ludi.heaps.effects.RoundedRectangleShader;
+
 class TestGridApp extends hxd.App {
+
+    var bmp:Bitmap;
 
     override function init() {
         super.init();
@@ -20,6 +26,7 @@ class TestGridApp extends hxd.App {
         Res.initEmbed();
      //   s2d.defaultSmooth = true;
 
+     /*
         var fxaa =  new FXAAFilter() ;
         fxaa.blendStrength = 3;
         s2d.filter = fxaa;
@@ -39,13 +46,35 @@ class TestGridApp extends hxd.App {
         grid.placeActor(actor, new Cell(0, 0));
         */
 
-        var builder = ludi.heaps.box.Box.build(300, 300);
+     /*   var builder = ludi.heaps.box.Box.build(300, 300);
         builder.roundedCorners(30);
         builder.verticalGradient(0xEC1515, 0x692D2D);
         var box = builder.get();
                      
 
-        s2d.addChild(box);
+        s2d.addChild(box);*/
+
+
+        var w = 240;
+        var h = 120;
+        bmp = new Bitmap(Tile.fromColor(0xFFFFFFFF, w, h), s2d);
+
+        // fill: ARGB, border optional
+        var shader = new RoundedRectangleShader(
+            w, h,
+            16,            // corner radius (px)
+            0xFF2D8CFF,    // fill ARGB
+            2,             // border thickness (px), 0 disables
+            0xFFFFFFFF,    // border ARGB
+            1.0            // AA feather (px)
+        );
+        bmp.addShader(shader);
+    }
+
+    public override function update(dt:Float) {
+        //slowly zoom in on bmp
+        bmp.scaleX += 0.01;
+        bmp.scaleY += 0.01;
     }
 
     public static function main() {
